622a8bda052101912606a305f9a9c516aa2d6b84
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-03-10 11:06:11 -0300

    Resolução problema Jackpot

---
+++ jackpot.js
@@ -0,0 +1,8 @@
+function solucao(resultado1, resultado2, resultado3) {
+	// seu código aqui
+    if (resultado2 === resultado1 && resultado3 === resultado1 ) {
+        return console.log(true)
+    }
+
+    return console.log(false)
+}
